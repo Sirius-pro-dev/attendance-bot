397cbd81a4e294e804b5582969ca0c7966f2aeff
Daniel <dan.usov82@gmail.com>
2023-11-16 10:11:46 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Telegraf } from 'telegraf';
+import 'dotenv/config';
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.bot.start((ctx) => ctx.reply('hi'));
+
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+bot.launch();
